42174a86fd330c7091439f4391a5ad7363f24901
Stefan Holzmüller <stefanholzmueller@github>
2014-04-15 15:37:26 +0200

    better AST type hierarchy

--- src/main/scala/stefanholzmueller/compiler/AST.scala
+++ src/main/scala/stefanholzmueller/compiler/AST.scala
@@ -3,6 +3,8 @@ package stefanholzmueller.compiler
 import scala.util.parsing.input.Positional
 
 sealed trait AST extends AbstractSyntaxTree with Positional
-case class BoolLiteral(value: Boolean) extends AST
-case class IntLiteral(value: Int) extends AST
-case class Variable(name: String) extends AST
+trait Expression extends AST
+trait Literal extends Expression
+case class BoolLiteral(value: Boolean) extends Literal
+case class IntLiteral(value: Int) extends Literal
+case class Variable(name: String) extends Expression
